99394ef75218652a8a4aac540eae09e677ddad9e
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2025-11-12 22:18:01 -0800

    install script runs properly

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+pip_install_from_git_repo() {
+    repo="$1"
+    commit="${2:main}"
+    dirname=$(basename $repo)
+    [ ! -d "$dirname" ] && git clone "$repo"
+    pushd "$dirname"
+    git fetch origin "$commit"
+    git checkout "$commit"
+    python -m pip install -e .
+    popd
+}
+
 if [ ! $(which uv) ]; then
     echo "installing uv"
     curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -20,14 +32,23 @@ if [ ! -d "BEHAVIOR-1K" ]; then
 
     uv run python -m ensurepip
     uv run python -m pip install pip --upgrade
- 
+
+    source .venv/bin/activate
+
+    pushd BEHAVIOR-1K
+    # Fix some install issues
+    python -m pip install pymeshlab~=2022.2
+    python -m pip install setuptools<=79
+    pip_install_from_git_repo https://github.com/StanfordVL/curobo cbaf7d32436160956dad190a9465360fad6aba73
+    pip_install_from_git_repo https://github.com/huggingface/lerobot 577cd10974b84bea1f06b6472eb9e5e74e07f77a
+
+    pushd bddl
+    python -m pip install -e .
+    popd
+
+    ./setup.sh --omnigibson --bddl --primitives --confirm-no-conda --accept-nvidia-eula --accept-dataset-tos
+    popd
+
 fi 
 
-if [ 1 ]; then
-    # cd BEHAVIOR-1K
-
-    cd BEHAVIOR-1K
-    uv run ./setup.sh --omnigibson --bddl --dataset --primitives --confirm-no-conda --accept-nvidia-eula --accept-dataset-tos
-    cd ..
-fi
 

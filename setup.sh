cce18b6a871b6d9ea3cb37a6a418ef348704c134
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2026-01-16 16:01:17 +0000

    remove behavior from setup for now

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ if [ ! $(which uv) ]; then
 fi
 
 uv sync
-wget https://raw.githubusercontent.com/facebookresearch/CrypTen/refs/heads/main/configs/default.yaml -P .venv/lib/python3.10/site-packages/configs/
+# wget https://raw.githubusercontent.com/facebookresearch/CrypTen/refs/heads/main/configs/default.yaml -P .venv/lib/python3.10/site-packages/configs/
 
 if [ ! -d "mp-spdz-0.4.1" ]; then
     echo "installing MP-SPDZ"
@@ -26,30 +26,30 @@ if [ ! -d "mp-spdz-0.4.1" ]; then
     tar -xvf mp-spdz-0.4.1.tar.xz
 fi
 
-if [ ! -d "BEHAVIOR-1K" ]; then
-    echo "installing Behavior-1K"
-    # Clone the latest stable release (recommended)
-    git clone -b v3.7.1 https://github.com/StanfordVL/BEHAVIOR-1K.git
+# if [ ! -d "BEHAVIOR-1K" ]; then
+#     echo "installing Behavior-1K"
+#     # Clone the latest stable release (recommended)
+#     git clone -b v3.7.1 https://github.com/StanfordVL/BEHAVIOR-1K.git
 
-    uv run python -m ensurepip
-    uv run python -m pip install pip --upgrade
+#     uv run python -m ensurepip
+#     uv run python -m pip install pip --upgrade
 
-    source .venv/bin/activate
+#     source .venv/bin/activate
 
-    pushd BEHAVIOR-1K
-    # Fix some install issues
-    python -m pip install pymeshlab~=2022.2
-    python -m pip install 'setuptools<=79'
-    pip_install_from_git_repo https://github.com/StanfordVL/curobo cbaf7d32436160956dad190a9465360fad6aba73
-    pip_install_from_git_repo https://github.com/huggingface/lerobot 577cd10974b84bea1f06b6472eb9e5e74e07f77a
+#     pushd BEHAVIOR-1K
+#     # Fix some install issues
+#     python -m pip install pymeshlab~=2022.2
+#     python -m pip install 'setuptools<=79'
+#     pip_install_from_git_repo https://github.com/StanfordVL/curobo cbaf7d32436160956dad190a9465360fad6aba73
+#     pip_install_from_git_repo https://github.com/huggingface/lerobot 577cd10974b84bea1f06b6472eb9e5e74e07f77a
 
-    pushd bddl
-    python -m pip install -e .
-    popd
+#     pushd bddl
+#     python -m pip install -e .
+#     popd
 
-    ./setup.sh --omnigibson --bddl --primitives --confirm-no-conda --accept-nvidia-eula --accept-dataset-tos
-    popd
+#     ./setup.sh --omnigibson --bddl --primitives --confirm-no-conda --accept-nvidia-eula --accept-dataset-tos
+#     popd
 
-fi 
+# fi 
 
 
